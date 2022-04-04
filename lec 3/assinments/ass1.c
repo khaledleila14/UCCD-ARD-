212360f0b9874934a2d9230df2f2c9d67d1d28bd
khaledleila14 <khaldmohamed@gmailcom>
2022-04-05 01:34:47 +0200

    added lec 2,3

---
+++ lec 3/assinments/ass1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main ()
+{
+	int x=0 , y=0 , sum=0 ;
+	printf ("enter the first num");
+	scanf (" %d",&x);
+	printf ("enter the second num");
+	scanf (" %d",&y);
+	while (sum = x + y)
+	{
+		printf("the sum = %d\n",sum);
+	printf ("enter the first num");
+	scanf (" %d",&x);
+	printf ("enter the second num");
+	scanf (" %d",&y);
+	}
+	}
